5003e58530f0ada6e5dfaa7653fbedbe18649daf
MartinPucan <pucan.m@seznam.cz>
2021-03-10 19:00:52 +0100

    need fix router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,9 @@ const Header = () => {
                     <a className="navbar-brand" href="/">English words</a>
                 </div>
                 <div>
+                    <a className="navbar-brand" href="/">Dashboard</a>
+                </div>
+                <div>
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
