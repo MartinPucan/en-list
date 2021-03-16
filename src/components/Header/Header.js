49bb4643b7e2d2e2107a126fb3d29622b677acf8
MartinPucan <pucan.m@seznam.cz>
2021-03-16 23:34:28 +0100

    removed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,16 @@ const Header = () => {
                 <div>
                     <a className="navbar-brand" href="/">English words</a>
                 </div>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Words</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sentences</a>
+                        </li>
+                    </ul>
+                </div>
                 <div>
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
